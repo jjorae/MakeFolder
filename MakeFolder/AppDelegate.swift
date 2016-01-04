b54b97aa46ed66ea96b1d1c920180584af2dbecc
rhcho <rhcho@rockplace.co.kr>
2016-01-04 10:24:27 +0900

    Initial Commit

---
+++ MakeFolder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MakeFolder
+//
+//  Created by 조래혁 on 2016. 1. 4..
+//  Copyright © 2016년 JJORAE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MakeFolder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MakeFolder
+//
+//  Created by 조래혁 on 2016. 1. 4..
+//  Copyright © 2016년 JJORAE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
